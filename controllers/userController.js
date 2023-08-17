1b9122f01679f7b1a229f8ef92f4bf0edf52eca9
Amanda Duva <amanda.j.duva@gmail.com>
2023-08-17 13:24:29 -0400

    updated user controller for getUsers and updateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,36 @@ const { Thought, User } = require("../models");
 
 const userController = {
     // get all users
-    getUsers(req, res) {
-        User.find().then((users) => res.json(users)).catch((error) => res.status(500).json(error));
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            return res.json(users);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
     // get user by id and pupulate thought and friend data
 
     // post a new user
 
     // update a user by its id
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.id },
+                { $set: req.body },
+                { new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user' })
+            }
+
+            res.json(user);
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
 
     // remove user by its id 
         // bonus remove user's associated thoughts when deleted
