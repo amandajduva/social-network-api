41f296258a419cd7dd332621b9106abf66243e77
Amanda Duva <amanda.j.duva@gmail.com>
2023-08-17 12:07:51 -0400

    updated thought model

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { Schema, model, Types } = require("mongoose");
+// new Date().toLocaleDateString()
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            // Default value is set to a new ObjectId
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            // default: 
+            // get: 
+      },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        // default: 
+        // get: 
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+//   Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query. 
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model("Thought", thoughtSchema);
+  
+  module.exports = Thought;
